927e620ea6cc29ed0a0aa21b7a9c6fcc425de4b5
Javier Dominguez II <Javi7057@gmail.com>
2022-11-07 20:54:27 -0600

    inital setup with tailwindcss, still need to add google identity services

---
+++ src/components/Login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+// import google login
+import { useNavigate } from "react-router-dom";
+import { Fcgoogle } from "react-icons/fc";
+
+const Login = () => {
+  return <div>Login</div>;
+};
+
+export default Login;
